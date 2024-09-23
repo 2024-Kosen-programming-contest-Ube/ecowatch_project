eab862b48d41033b08a0d0698b16434b2bff06ee
Kogepan229 <kogepan25629@gmail.com>
2024-09-23 14:47:39 +0900

    Fix schema

--- damy_server/server.py
+++ damy_server/server.py
@@ -1,4 +1,5 @@
 # FastAPIの読み込み
+import random
 from fastapi import FastAPI
 from pydantic import BaseModel
 import math
@@ -9,11 +10,19 @@ app = FastAPI()
 
 @app.get("/data/getinfo")
 async def getinfo():
+    # return {
+    #     "temperature": 26.5,
+    #     "humidity": 60,
+    #     "isPeople": True,
+    #     "lux": 77.4,
+    #     "useairconditionaer": True,
+    #     "airconditionaertime": "2024-06-30-22:20:50"
+    # }
     return {
-        "temperature": 26.5,
-        "humidity": 60,
+        "temperature": random.randint(20, 40),
+        "humidity": random.randint(50, 100),
         "isPeople": True,
         "lux": 77.4,
         "useairconditionaer": True,
-        "airconditionaertime": "2024-06-30-22:20:50"
+        "airconditionaer_time": "2024-06-30-22:20:50"
     }
