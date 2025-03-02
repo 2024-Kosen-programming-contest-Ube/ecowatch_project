db48ab9487ec6d509114d17feee85d803d72d0c9
Kogepan229 <kogepan25629@gmail.com>
2025-03-03 01:11:26 +0900

    backend

--- scripts/start.sh
+++ scripts/start.sh
@@ -9,13 +9,20 @@ CURRENT=$(cd $(dirname $0);pwd)
 cd ${CURRENT}/..
 
 {
+  cd ./ecowatch_backend
+  ./ecowatch_backend
+} &
+PID1=$!
+echo ${PID1}
+
+{
   cd ./Device_GetInfoPy
   ./.venv/bin/python main.py
 } &
 PID2=$!
 echo ${PID2}
 
-trap 'kill ${PID2}' 2 3 15
+trap 'kill ${PID1};kill ${PID2}' 2 3 15
 
 # 終了待ち
 wait
